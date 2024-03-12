cbb8a9a06d5e16edd40750c78ad4ff93ded2cc7d
Sandip <sandipganava1234@gmail.com>
2024-03-12 09:45:43 +0530

    delete test file

--- test.js
+++
@@ -1,7 +0,0 @@
-const validate = require('./index');
-
-async function validateNotEmpty() {
-    const email = await validate.validateURL('https://github.com/Anuj-Kumar-Sharma/cs-currency-converter/blob/mastjjer/index.js')
-    console.log("validate", email)
-}
-validateNotEmpty()
